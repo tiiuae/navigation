d201d36504de7a1e09536862b49782509f4d7f69
Jari Nippula <jarix@ssrc.tii.ae>
2022-02-02 11:23:26 +0200

    entrypoint.sh: Check SIMULATION var for use_sim_time flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 source /opt/ros/galactic/setup.bash
-ros2 launch navigation navigation.py
+
+ROS_FLAGS=""
+if [ ${SIMULATION+x} != "" ]; then
+    ROS_FLAGS="use_sim_time:=true ${ROS_FLAGS}"
+fi
+
+ros2 launch navigation navigation.py ${ROS_FLAGS}
